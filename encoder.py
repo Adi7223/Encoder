07700c72ca92d378f4792c079b5dab8c52c8397c
Aditya <147325302+Adi7223@users.noreply.github.com>
2024-05-07 23:26:39 +0530

    initial commit

---
+++ encoder.py
@@ -0,0 +1,30 @@
+st = input("Enter message :")
+words = st.split(" ")
+print(words)
+coding = input("Encode or Decode : ")
+# if (coding=="Encode"or"encode"): 
+#  print("Encoding...")
+if(coding=="encode"):
+  print("Encoding...")
+  nwords = []
+  for word in words:
+    if(len(word)>=3):
+      r1 = "dsf"
+      r2 = "jkr"
+      stnew = r1+ word[1:] + word[0] + r2
+      nwords.append(stnew)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
+
+else:
+  print("Decoding...")
+  nwords = []
+  for word in words:
+    if(len(word)>=3): 
+      stnew = word[3:-3]
+      stnew = stnew[-1] + stnew[:-1]
+      nwords.append(stnew)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
